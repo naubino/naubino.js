72c076ba2cb9e517da7a767f84751db53b8ab204
Hendrik Sollich <hendrik.sollich@gmx.de>
2011-04-18 10:59:09 +0200

    modified sidescrollers build system

--- js/includes.js
+++ js/includes.js
@@ -3,8 +3,4 @@ var include = function(file, path) {
     var args = 'type="text/javascript" src="'+path+file+'.js"';
     document.write('<script '+args+'></script>');
 };
-var toinc = ["glu", "Game", "Naub", "NaubShape", "PhysicsModel","NaubShape"];
-for (var i in toinc) include(toinc[i]);
 include("b2Vec2", "js/");
-
-window.naubino = {};
