838a0f8f3f9a89b2e4355e90e2694e516359edb2
Hendrik Sollich <Hendrik@hoodie.de>
2011-09-21 21:35:40 +0200

    cleaned up root

--- setup-repo.sh
+++ setup-repo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git submodule update --init lib/my-common/
 git submodule sync
-mkdir javascripts
-coffee -o javascripts/ -cb coffee/*.coffee
+mkdir js
+coffee -o js/ -cb coffee/*.coffee
